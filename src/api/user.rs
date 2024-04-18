af9761b3732a5a3ee913c42864b16b7683772228
Yoonge <7884008@qq.com>
2024-04-18 18:14:52 +0800

    feat: ✨ implement `uuid` & `hashed password`
    Co-authored-by: Svelte <dev.yoonge@gmail.com>

--- src/api/user.rs
+++ src/api/user.rs
@@ -2,11 +2,14 @@ use axum::{
     extract::{Path, State},
     Json,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Postgres};
+use time::OffsetDateTime;
+use uuid::Uuid;
 
 use super::ApiError;
-use conduit_axum::db::User;
+use conduit_axum::db::{date_formatter, User};
+use conduit_axum::password;
 
 #[derive(Deserialize)]
 pub struct NewUser {
@@ -15,20 +18,39 @@ pub struct NewUser {
     pub username: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct QueryUser {
+    pub id: Uuid,
+    pub avatar: String,
+    pub bio: String,
+    pub birthday: String,
+    #[serde(with = "date_formatter")]
+    pub create_at: OffsetDateTime,
+    pub email: String,
+    pub favorite: Vec<Uuid>,
+    pub gender: i16,
+    pub nickname: String,
+    pub phone: String,
+    pub position: String,
+    pub username: String,
+}
+
 pub async fn create_user(
     State(pool): State<Pool<Postgres>>,
     Json(new_user): Json<NewUser>,
 ) -> Result<Json<User>, ApiError> {
+    let hashed_password = password::hash(new_user.password).await?;
     let user = sqlx::query_as!(
         User,
-        "insert into users (email, password, username) values ($1, $2, $3) returning *",
+        r#"
+            insert into users (email, password, username)
+            values ($1, $2, $3)
+            returning *
+        "#,
         &new_user.email,
-        &new_user.password,
+        &hashed_password,
         &new_user.username
     )
-    // .bind(&new_user.email)
-    // .bind(&new_user.password)
-    // .bind(&new_user.username)
     .fetch_one(&pool)
     .await?;
 
@@ -39,23 +61,46 @@ pub async fn create_user(
 
 pub async fn get_user(
     State(pool): State<Pool<Postgres>>,
-    Path(user_id): Path<i32>,
-) -> Result<Json<User>, ApiError> {
-    let user = sqlx::query_as!(User, "select * from users where id = $1", user_id)
-        .fetch_one(&pool)
-        .await?;
+    Path(username): Path<String>,
+) -> Result<Json<QueryUser>, ApiError> {
+    let user = sqlx::query_as!(
+        QueryUser,
+        r#"
+            select id, avatar, bio, birthday, create_at, email, favorite, gender, nickname, phone, position, username
+            from users
+            where username = $1
+        "#,
+        username
+    )
+    .fetch_one(&pool)
+    .await?;
 
     println!("{:?}", user);
 
     Ok(Json(user))
 }
 
-pub async fn get_users(State(pool): State<Pool<Postgres>>) -> Result<Json<Vec<User>>, ApiError> {
-    let users = sqlx::query_as!(User, "select * from users")
-        .fetch_all(&pool)
-        .await?;
+pub async fn get_users(
+    State(pool): State<Pool<Postgres>>,
+) -> Result<Json<Vec<QueryUser>>, ApiError> {
+    let users = sqlx::query_as!(
+        QueryUser,
+        r#"
+            select id, avatar, bio, birthday, create_at, email, favorite, gender, nickname, phone, position, username
+            from users
+        "#
+    )
+    .fetch_all(&pool)
+    .await?;
 
     println!("{:?}", users);
 
     Ok(Json(users))
+}
+
+pub async fn verify_pwd(password: String) -> Result<String, ApiError> {
+    let pwd = password::hash(password.clone()).await?;
+    let res = password::verify(password, pwd).await?;
+
+    Ok(res.to_string())
 }
